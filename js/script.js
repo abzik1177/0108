2cc3a138aeb319001c018103f05ad32f432f126d
Abzal <abzik1177@gmail.com>
2020-08-01 19:12:22 +0500

    second commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const numberOfFilms = +prompt('Skolko filmov vi stomreli', ''); 
 
 const personalMovieDB = {
@@ -18,3 +20,39 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+if (4 == 4 ) {
+    console.log('OK!');
+} else {
+    console.log('Error');
+}
+
+/*const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+}else if (num > 99) {
+    console.log('Mnogo');
+}else {
+    console.log('Ok');
+}
+
+(num ===50) ? console.log("'ok!") : console.log('Error');
+*/
+
+const num = 49;
+
+switch (num) {
+    case 49:
+        console.log('Yoq teparoq');
+        break;
+    case 100:
+        console.log('Yoq pasroq');
+        break;
+    case 50:
+        console.log('V tochku');
+        break; 
+    default:
+        console.log('Yoq bu safar emas');
+        break;
+}
